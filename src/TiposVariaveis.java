e4dd1d63c2c297ae4d6cfb0ede5897b6da5f1591
LipeOzyy <fhenriquenatal@gmail.com>
2024-06-16 17:22:24 -0300

    nada demais

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,20 @@
+
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int a, b;
+
+        a = 5;
+        b=6;
+
+        String resultado = "";
+        if(a == b)
+            resultado = "Verdadeiro";
+        else
+            resultado = "Falso";
+
+        System.out.println(resultado);
+
+       
     }
 }
